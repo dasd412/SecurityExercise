683f576e45a34344b8d111d6b1db813a39cc81ad
dasd <dasd412@naver.com>
2021-12-28 13:22:33 +0900

    시큐리티 실습 환경 설정

---
+++ src/main/java/com/dasd/config/WebMvcConfig.java
@@ -0,0 +1,22 @@
+package com.dasd.config;
+
+import org.springframework.boot.web.servlet.view.MustacheViewResolver;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    //.html 파일을 해당 설정으로 되있는 mustache 파일로 인식하게 함.
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        MustacheViewResolver resolver = new MustacheViewResolver();
+        resolver.setCharset("UTF-8");
+        resolver.setContentType("text/html;charset=UTF-8");
+        resolver.setPrefix("classpath:/templates/");
+        resolver.setSuffix(".html");
+
+        registry.viewResolver(resolver);
+    }
+}
